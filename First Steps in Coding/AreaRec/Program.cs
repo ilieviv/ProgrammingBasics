8252a44cd613d85f78b4ae9ceacbc2e70b300b17
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-02-03 15:55:02 +0200

    HW1 done

---
+++ First Steps in Coding/AreaRec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(a*b);
+        }
+    }
+}
---
+++ First Steps in Coding/Nums1to20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nums1to20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
