3571acd1126ee55ae8d06e938090c8df00c6602f
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-02-03 14:24:14 +0200

    first task done

---
+++ First Steps in Coding/First Steps in Coding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Steps_in_Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
