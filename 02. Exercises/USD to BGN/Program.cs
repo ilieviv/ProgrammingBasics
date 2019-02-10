30a5cd35637de87721d5c8028bea7201d5dffaa5
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-02-10 10:52:20 +0200

    exercise

---
+++ 02. Exercises/USD to BGN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace USD_to_BGN
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double usd = double.Parse(Console.ReadLine());
+			double fixing = 1.79549;
+
+			//double bgn = usd * fixing;
+			double bgn = Math.Round(usd * fixing, 2);
+
+			Console.WriteLine("{0:f2}", bgn);
+		}
+	}
+}
