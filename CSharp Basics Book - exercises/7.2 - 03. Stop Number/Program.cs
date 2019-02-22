b666834b6f940b281e90fab8c4c52413d040e3d2
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-02-22 12:05:04 +0200

    7.2 all done

---
+++ CSharp Basics Book - exercises/7.2 - 03. Stop Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _7._2___03._Stop_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            do
+            {
+                if (m % 2 == 0 && m % 3 == 0)
+                {
+                    if (m == s)
+                    {
+                        break;
+                    }
+                    Console.Write("{0} ", m);
+                }
+                m--;
+            } while (m >= n);
+            Console.WriteLine();
+
+            //for (int i = m; i >= n; i--)
+            //{
+            //    if (i%2==0 && i%3==0)
+            //    {
+            //        if (i==s)
+            //        {
+            //            break;
+            //        }
+            //        Console.Write("{0} ", i);
+            //    }
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ CSharp Basics Book - exercises/7.2 - 05. Digits/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _7._2___05._Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            var numCopy = num;
+            int d3 = numCopy % 10;
+            numCopy /= 10;
+            int d2 = numCopy % 10;
+            numCopy /= 10;
+            int d1 = numCopy % 10;
+
+            for (int i = 0; i < d1 + d2; i++)
+            {
+                for (int j = 0; j < d1 + d3; j++)
+                {
+                    if (num % 5 == 0)
+                    {
+                        num -= d1;
+                        Console.Write("{0} ", num);
+                    }
+                    else if (num % 3 == 0)
+                    {
+                        num -= d2;
+                        Console.Write("{0} ", num);
+                    }
+                    else
+                    {
+                        num += d3;
+                        Console.Write("{0} ", num);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
