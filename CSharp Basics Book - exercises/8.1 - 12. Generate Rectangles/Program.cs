c62a64bd06dd95bca1d311f2de92e301bad16ff0
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-03-01 12:39:22 +0200

    8.1 all done

--- CSharp Basics Book - exercises/8.1 - 12. Generate Rectangles/Program.cs
+++ CSharp Basics Book - exercises/8.1 - 12. Generate Rectangles/Program.cs
@@ -6,10 +6,35 @@ namespace _8._1___12._Generate_Rectangles
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
 
+            int count = 0;
+            int area = 0;
 
+            for (int left = -n; left < n; left++)
+            {
+                for (int top = -n; top < n; top++)
+                {
+                    for (int right = left + 1; right <= n; right++)
+                    {
+                        for (int bottom = top + 1; bottom <= n; bottom++)
+                        {
+                            area = Math.Abs(right - left) * Math.Abs(bottom - top);
+
+                            if (area >= m)
+                            {
+                                Console.WriteLine("({0}, {1}) ({2}, {3}) -> {4}", left, top, right, bottom, area);
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
