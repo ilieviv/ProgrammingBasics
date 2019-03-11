2c9e11d67f45ef7c309b0136aa7728558d1a2031
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-03-11 11:33:15 +0200

    e

---
+++ CSharp Principles Book/01 exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
