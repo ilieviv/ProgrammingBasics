712cebfe8d6bc23247258ef66f469ca4872732e7
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-03-14 21:32:28 +0200

    exercise added

--- CSharp Principles Book/05 - array sort 5 numbers/Program.cs
+++ CSharp Principles Book/05 - array sort 5 numbers/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Numerics;
 
-namespace _01_exercise
+namespace _05___array_sort_5_numbers
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
